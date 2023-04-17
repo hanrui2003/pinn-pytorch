82954a547b362841eb4bbfc85881f64c1591a8ed
liqingchun <liqingchun@mumatech.cn>
2023-04-17 10:47:26 +0800

    DeepONet

--- lorenz63/deeponet/sin_don_01_test.py
+++ lorenz63/deeponet/sin_don_01_test.py
@@ -9,7 +9,10 @@ from sin_don_01 import PIDeepONet
 PIDON = torch.load('sin_don_01.pt')
 print(PIDON)
 
-initial = torch.tensor(0.3)
+# 对于参数，也是有约束的，测试的参数，要符合训练的时候的参数分布
+# 比如这个例子中，训练的时候，参数（初值）的取值范围是[0,1)，那么在测试的时候也只能选择一个[0,1)之间的数
+# 但即使不选择[0,1)之间的数，算子网络的输出至少不会混乱，也是会向真解靠拢。
+initial = torch.tensor(2.3)
 
 # 测试点
 n_t = 600
