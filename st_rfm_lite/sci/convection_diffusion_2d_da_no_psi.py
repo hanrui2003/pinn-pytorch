b4dc85c5ee6b0fb7fc86f3f4c79d04ed4a2f73a6
liqingchun <merlini1988@126.com>
2024-08-03 21:27:14 +0800

    sci-rfm

--- st_rfm_lite/sci/convection_diffusion_2d_da_no_psi.py
+++ st_rfm_lite/sci/convection_diffusion_2d_da_no_psi.py
@@ -11,21 +11,22 @@ from datetime import datetime
 
 torch.set_default_dtype(torch.float64)
 Pi = np.pi
-nu = 0.01
-v = 0.01
+v_x = 0.01
+v_y = 0.01
 D = 1
 
 
-def u_real(x, t):
-    return np.exp(-t) * np.sin(Pi * (x + t + x * t))
+def u_real(x, y, t):
+    return np.exp(-t) * np.sin(Pi * (x + y + t + x * y * t))
 
 
-def f_real(x, t):
-    u = np.exp(-t) * np.sin(Pi * (x + t + x * t))
-    u_x = np.exp(-t) * Pi * (t + 1) * np.cos(Pi * (x + t + x * t))
-    u_xx = -np.exp(-t) * (Pi * (t + 1)) ** 2 * np.sin(Pi * (x + t + x * t))
-    u_t = np.exp(-t) * (Pi * (x + 1) * np.cos(Pi * (x + t + x * t)) - np.sin(Pi * (x + t + x * t)))
-    return u_t + v * u_x - nu * u_xx + D * u
+def f_real(x, y, t):
+    u_x = np.exp(-t) * Pi * (y * t + 1) * np.cos(Pi * (x + y + t + x * y * t))
+    u_xx = -np.exp(-t) * (Pi * (y * t + 1)) ** 2 * np.sin(Pi * (x + y + t + x * y * t))
+    u_y = np.exp(-t) * Pi * (x * t + 1) * np.cos(Pi * (x + y + t + x * y * t))
+    u_yy = -np.exp(-t) * (Pi * (x * t + 1)) ** 2 * np.sin(Pi * (x + y + t + x * y * t))
+    u_t = np.exp(-t) * (Pi * (x * y + 1) * np.cos(Pi * (x + y + t + x * y * t)) - np.sin(Pi * (x + y + t + x * y * t)))
+    return u_t + v_x * u_x + v_y * u_y - D * (u_xx + u_yy)
 
 
 def L_inf_error(v, axis=None):
@@ -46,58 +47,67 @@ def weights_init(m):
 
 
 class LocalNet(nn.Module):
-    def __init__(self, in_features, hidden_features, x_max, x_min, t_max, t_min):
+    def __init__(self, in_features, hidden_features, x_max, x_min, y_max, y_min, t_max, t_min):
         super(LocalNet, self).__init__()
         self.in_features = in_features
         self.hidden_features = hidden_features
         # 半径的倒数
-        self.r_inv = torch.tensor([2.0 / (x_max - x_min), 2.0 / (t_max - t_min)])
+        self.r_inv = torch.tensor([2.0 / (x_max - x_min), 2.0 / (y_max - y_min), 2.0 / (t_max - t_min)])
         # 区域中心
-        self.y_c = torch.tensor([(x_max + x_min) / 2, (t_max + t_min) / 2])
+        self.z_c = torch.tensor([(x_max + x_min) / 2, (y_max + y_min) / 2, (t_max + t_min) / 2])
         self.phi = nn.Sequential(nn.Linear(self.in_features, self.hidden_features, bias=True), nn.Tanh())
 
-    def forward(self, y):
+    def forward(self, z):
         # 标准化，使得取值在[-1,1]
-        y = (y - self.y_c) * self.r_inv
-        return self.phi(y)
+        z = (z - self.z_c) * self.r_inv
+        return self.phi(z)
 
 
-def pre_define(Nx, Nt, M, X_min, X_max, T_min, T_max):
+def pre_define(Nx, Ny, Nt, M, X_min, X_max, Y_min, Y_max, T_min, T_max):
     print(datetime.now(), "pre_define start")
     models = []
     delta_x = (X_max - X_min) / Nx
+    delta_y = (Y_max - Y_min) / Ny
     delta_t = (T_max - T_min) / Nt
     for k in range(Nx):
         model_for_x = []
         x_min = k * delta_x + X_min
         x_max = (k + 1) * delta_x + X_min
-        for n in range(Nt):
-            t_min = n * delta_t + T_min
-            t_max = (n + 1) * delta_t + T_min
-            model = LocalNet(in_features=2, hidden_features=M, x_min=x_min, x_max=x_max, t_min=t_min, t_max=t_max)
-            model = model.apply(weights_init)
-            for param in model.parameters():
-                param.requires_grad = False
-            model_for_x.append(model)
+        for j in range(Ny):
+            model_for_xy = []
+            y_min = j * delta_y + Y_min
+            y_max = (j + 1) * delta_y + Y_min
+            for n in range(Nt):
+                t_min = n * delta_t + T_min
+                t_max = (n + 1) * delta_t + T_min
+                model = LocalNet(in_features=3, hidden_features=M, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
+                                 t_min=t_min, t_max=t_max)
+                model = model.apply(weights_init)
+                for param in model.parameters():
+                    param.requires_grad = False
+                model_for_xy.append(model)
+
+            model_for_x.append(model_for_xy)
+
         models.append(model_for_x)
     print(datetime.now(), "pre_define end")
     return models
 
 
-def cal_matrix(models, Nx, Nt, M, Qx, Qt, pde_points, ic_points, bc_points, obs_points):
+def cal_matrix(models, Nx, Ny, Nt, M, Qx, Qy, Qt, pde_points, ic_points, bc_points, obs_points):
     print(datetime.now(), "cal_matrix start")
     # matrix define (Aw=b)
     # pde配点
-    A_P = np.zeros((len(pde_points), Nx * Nt * M))
+    A_P = np.zeros((len(pde_points), Nx * Ny * Nt * M))
 
     # 初值配点
-    A_I = np.zeros((len(ic_points), Nx * Nt * M))
+    A_I = np.zeros((len(ic_points), Nx * Ny * Nt * M))
 
     # 边界的配点
-    A_B = np.zeros((len(bc_points), Nx * Nt * M))
+    A_B = np.zeros((len(bc_points), Nx * Ny * Nt * M))
 
     # 观测点
-    A_O = np.zeros((len(obs_points), Nx * Nt * M))
+    A_O = np.zeros((len(obs_points), Nx * Ny * Nt * M))
 
     pde_point = torch.tensor(pde_points, requires_grad=True)
     ic_point = torch.tensor(ic_points, requires_grad=True)
@@ -105,57 +115,65 @@ def cal_matrix(models, Nx, Nt, M, Qx, Qt, pde_points, ic_points, bc_points, obs_
     obs_point = torch.tensor(obs_points, requires_grad=True)
 
     for k in range(Nx):
-        for n in range(Nt):
-            M_begin = (k * Nt + n) * M
-            pde_out = models[k][n](pde_point)
-            pde_values = pde_out.detach().numpy()
+        for j in range(Ny):
+            for n in range(Nt):
+                M_begin = (k * Ny * Nt + j * Nt + n) * M
+                pde_out = models[k][j][n](pde_point)
+                pde_values = pde_out.detach().numpy()
 
-            bc_out = models[k][n](bc_point)
-            bc_values = bc_out.detach().numpy()
+                bc_out = models[k][j][n](bc_point)
+                bc_values = bc_out.detach().numpy()
 
-            ic_out = models[k][n](ic_point)
-            ic_values = ic_out.detach().numpy()
+                ic_out = models[k][j][n](ic_point)
+                ic_values = ic_out.detach().numpy()
 
-            obs_out = models[k][n](obs_point)
-            obs_values = obs_out.detach().numpy()
+                obs_out = models[k][j][n](obs_point)
+                obs_values = obs_out.detach().numpy()
 
-            grad_u_x = []
-            grad_u_t = []
-            grad_u_xx = []
-            for i in range(M):
-                u_x_t = autograd.grad(outputs=pde_out[:, i], inputs=pde_point,
-                                      grad_outputs=torch.ones_like(pde_out[:, i]),
-                                      create_graph=True, retain_graph=True)[0]
-                u_x = u_x_t[:, 0]
-                u_t = u_x_t[:, 1]
+                grad_u_x = []
+                grad_u_y = []
+                grad_u_t = []
+                grad_u_xx = []
+                grad_u_yy = []
+                for i in range(M):
+                    u_x_y_t = autograd.grad(outputs=pde_out[:, i], inputs=pde_point,
+                                            grad_outputs=torch.ones_like(pde_out[:, i]),
+                                            create_graph=True, retain_graph=True)[0]
+                    u_x = u_x_y_t[:, 0]
+                    u_y = u_x_y_t[:, 1]
+                    u_t = u_x_y_t[:, 2]
 
-                u_xx_xt = autograd.grad(outputs=u_x, inputs=pde_point,
-                                        grad_outputs=torch.ones_like(u_x),
-                                        create_graph=True, retain_graph=True)[0]
-                u_xx = u_xx_xt[:, 0]
+                    u_xx_xy_xt = autograd.grad(outputs=u_x, inputs=pde_point,
+                                               grad_outputs=torch.ones_like(u_x),
+                                               create_graph=True, retain_graph=True)[0]
+                    u_xx = u_xx_xy_xt[:, 0]
 
-                grad_u_x.append(u_x.detach().numpy())
-                grad_u_t.append(u_t.detach().numpy())
-                grad_u_xx.append(u_xx.detach().numpy())
+                    u_yx_yy_yt = autograd.grad(outputs=u_y, inputs=pde_point,
+                                               grad_outputs=torch.ones_like(u_y),
+                                               create_graph=True, retain_graph=True)[0]
+                    u_yy = u_yx_yy_yt[:, 1]
 
-            grad_u_x = np.array(grad_u_x).T
-            grad_u_t = np.array(grad_u_t).T
-            grad_u_xx = np.array(grad_u_xx).T
+                    grad_u_x.append(u_x.detach().numpy())
+                    grad_u_y.append(u_y.detach().numpy())
+                    grad_u_t.append(u_t.detach().numpy())
+                    grad_u_xx.append(u_xx.detach().numpy())
+                    grad_u_yy.append(u_yy.detach().numpy())
 
-            A_P[:, M_begin: M_begin + M] = grad_u_t + v * grad_u_x - nu * grad_u_xx + D * pde_values
-            A_I[:, M_begin: M_begin + M] = ic_values
-            A_B[:, M_begin: M_begin + M] = bc_values
-            A_O[:, M_begin: M_begin + M] = obs_values
+                grad_u_x = np.array(grad_u_x).T
+                grad_u_y = np.array(grad_u_y).T
+                grad_u_t = np.array(grad_u_t).T
+                grad_u_xx = np.array(grad_u_xx).T
+                grad_u_yy = np.array(grad_u_yy).T
 
-    f_P = f_real(pde_points[:, [0]], pde_points[:, [1]])
-    interp_point = np.load('convection_diffusion_interp.npz')
-    x_interp = interp_point['x_interp']
-    y_interp = interp_point['y_interp']
-    y_noise = np.interp(ic_points[:, [0]], x_interp, y_interp)
-    y_noise_bc = np.interp(bc_points[:, [1]], x_interp, y_interp)
-    f_I = u_real(ic_points[:, [0]], ic_points[:, [1]]) + y_noise
-    f_B = u_real(bc_points[:, [0]], bc_points[:, [1]]) + y_noise_bc
-    f_O = u_real(obs_points[:, [0]], obs_points[:, [1]])
+                A_P[:, M_begin: M_begin + M] = grad_u_t + v_x * grad_u_x + v_y * grad_u_y - D * (grad_u_xx + grad_u_yy)
+                A_I[:, M_begin: M_begin + M] = ic_values
+                A_B[:, M_begin: M_begin + M] = bc_values
+                A_O[:, M_begin: M_begin + M] = obs_values
+
+    f_P = f_real(pde_points[:, [0]], pde_points[:, [1]], pde_points[:, [2]])
+    f_I = u_real(ic_points[:, [0]], ic_points[:, [1]], ic_points[:, [2]])
+    f_B = u_real(bc_points[:, [0]], bc_points[:, [1]], bc_points[:, [2]])
+    f_O = u_real(obs_points[:, [0]], obs_points[:, [1]], obs_points[:, [2]])
 
     c_p = 100.0
     c_i = 1e-5
@@ -198,27 +216,33 @@ def main(Nx, Ny, Nt, M, Qx, Qy, Qt):
     # 用于计算pde损失的点
     pde_points = np.hstack((X.flatten()[:, None], Y.flatten()[:, None], T.flatten()[:, None]))
     # 带标签值的点，即有真解的点
-    ic_points = np.hstack((X[0][:, None], T[0][:, None]))
-    left_bc_points = np.hstack((X[:, 0][:, None], T[:, 0][:, None]))
-    right_bc_points = np.hstack((X[:, -1][:, None], T[:, -1][:, None]))
-    bc_points = np.vstack((left_bc_points, right_bc_points))
+    ic_points = np.hstack((X[:, :, 0].flatten()[:, None], Y[:, :, 0].flatten()[:, None], T[:, :, 0].flatten()[:, None]))
+    top_bc_points = np.hstack((X[0].flatten()[:, None], Y[0].flatten()[:, None], T[0].flatten()[:, None]))
+    bottom_bc_points = np.hstack((X[-1].flatten()[:, None], Y[-1].flatten()[:, None], T[-1].flatten()[:, None]))
+    left_bc_points = np.hstack(
+        (X[:, 0, :].flatten()[:, None], Y[:, 0, :].flatten()[:, None], T[:, 0, :].flatten()[:, None]))
+    right_bc_points = np.hstack(
+        (X[:, -1, :].flatten()[:, None], Y[:, -1, :].flatten()[:, None], T[:, -1, :].flatten()[:, None]))
+    bc_points = np.vstack((top_bc_points, bottom_bc_points, left_bc_points, right_bc_points))
 
     t_obs = np.linspace(T_min, T_max, 11)[1:]
     x_obs = np.linspace(X_min, X_max, 51)[1:-1]
-    obs_points = np.array([(x, t) for x in x_obs for t in t_obs])
+    y_obs = np.linspace(Y_min, Y_max, 51)[1:-1]
+    obs_points = np.array([(x, y, t) for x in x_obs for y in y_obs for t in t_obs])
 
-    models = pre_define(Nx=Nx, Nt=Nt, M=M, X_min=X_min, X_max=X_max, T_min=T_min, T_max=T_max)
+    models = pre_define(Nx=Nx, Ny=Ny, Nt=Nt, M=M, X_min=X_min, X_max=X_max, Y_min=Y_min, Y_max=Y_max, T_min=T_min,
+                        T_max=T_max)
 
     # matrix define (Aw=b)
-    A, f = cal_matrix(models, Nx, Nt, M, Qx, Qt, pde_points, ic_points, bc_points, obs_points)
+    A, f = cal_matrix(models, Nx, Ny, Nt, M, Qx, Qy, Qt, pde_points, ic_points, bc_points, obs_points)
 
     # 为什么选择gelss，默认的不行吗？
     w = lstsq(A, f, lapack_driver="gelss")[0]
     print(datetime.now(), "main process end")
 
-    torch.save(models, 'convection_diffusion_da_no_psi.pt')
-    np.savez('convection_diffusion_da_no_psi.npz', w=w,
-             config=np.array([Nx, Nt, M, Qx, Qt, X_min, X_max, T_min, T_max], dtype=int))
+    torch.save(models, 'convection_diffusion_2d_da_no_psi.pt')
+    np.savez('convection_diffusion_2d_da_no_psi.npz', w=w,
+             config=np.array([Nx, Ny, Nt, M, Qx, Qy, Qt, X_min, X_max, Y_min, Y_max, T_min, T_max], dtype=int))
 
     print(datetime.now(), "main end")
 
@@ -229,27 +253,27 @@ if __name__ == '__main__':
     # np.random.seed(123)
 
     X_min = 0.0
-    X_max = 2.0
+    X_max = 5.0
     Y_min = 0.0
-    Y_max = 3.0
+    Y_max = 5.0
     T_min = 0.0
     T_max = 1.0
 
     # 以下是五组配置，每次训练只取一列
     # x维度划分的区间数
-    Nxs = [2, ]
+    Nxs = [5, ]
     # y维度划分的区间数
-    Nys = [3, ]
+    Nys = [5, ]
     # t维度划分的区间数
-    Nts = [1, ]
+    Nts = [2, ]
     # 每个局部局域的特征函数数量
-    Ms = [2, ]
+    Ms = [100, ]
     # x维度每个区间的配点数，Qx+1
-    Qxs = [2, ]
+    Qxs = [10, ]
     # y维度每个区间的配点数，Qx+1
-    Qys = [2, ]
+    Qys = [10, ]
     # t维度每个区间的配点数，Qt+1
-    Qts = [2, ]
+    Qts = [10, ]
 
     loop = zip(Nxs, Nys, Nts, Ms, Qxs, Qys, Qts)
     for i, item in enumerate(loop):
