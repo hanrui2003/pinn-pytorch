3b5387cd2ddf342a47ed4b5890287d0a3946dd28
liqingchun <merlini1988@126.com>
2024-08-07 21:39:34 +0800

    sci-rfm

--- st_rfm_lite/sci/convection_diffusion_2d_da_complex_domain_test.py
+++ st_rfm_lite/sci/convection_diffusion_2d_da_complex_domain_test.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.autograd as autograd
 import math
-from convection_diffusion_2d_da_complex_domain import u_real, LocalNet
+from convection_diffusion_2d_da_complex_domain import u_real, LocalNet, pick_point
 
 from scipy.linalg import lstsq
 from datetime import datetime
@@ -28,21 +28,25 @@ def plot(X, Y, U):
     ax1.plot_surface(X, Y, U[0], cmap="rainbow", vmin=min_value, vmax=max_value)
     ax1.set_xlabel('x')
     ax1.set_ylabel('y')
+    ax1.set_zlabel('u')
     ax1.set_zlim(min_value, max_value)
 
     ax2.plot_surface(X, Y, U[1], cmap="rainbow", vmin=min_value, vmax=max_value)
     ax2.set_xlabel('x')
     ax2.set_ylabel('y')
+    ax2.set_zlabel('u')
     ax2.set_zlim(min_value, max_value)
 
     ax3.plot_surface(X, Y, U[2], cmap="rainbow", vmin=min_value, vmax=max_value)
     ax3.set_xlabel('x')
     ax3.set_ylabel('y')
+    ax3.set_zlabel('u')
     ax3.set_zlim(min_value, max_value)
 
     ax4.plot_surface(X, Y, U[3], cmap="rainbow", vmin=min_value, vmax=max_value)
     ax4.set_xlabel('x')
     ax4.set_ylabel('y')
+    ax4.set_zlabel('u')
     ax4.set_zlim(min_value, max_value)
 
     plt.show()
@@ -81,18 +85,21 @@ circles = [
 if __name__ == '__main__':
     print(datetime.now(), "Main start")
 
-    data = np.load("convection_diffusion_2d_da_complex_domain_300.npz")
+    data = np.load("convection_diffusion_2d_da_complex_domain_250.npz")
     Nx, Ny, Nt, M, Qx, Qy, Qt, X_min, X_max, Y_min, Y_max, T_min, T_max = data['config']
     w = data['w']
 
-    models = torch.load('convection_diffusion_2d_da_complex_domain_300.pt')
+    models = torch.load('convection_diffusion_2d_da_complex_domain_250.pt')
 
     print(datetime.now(), "test start")
     test_Qx = 2 * Qx
     test_Qy = 2 * Qy
+    test_Qt = 2 * Qt
 
     x = np.linspace(X_min, X_max, Nx * test_Qx + 1)
     y = np.linspace(Y_min, Y_max, Ny * test_Qy + 1)
+    t = np.linspace(T_min, T_max, Nt * test_Qt + 1)
+
     X, Y = np.meshgrid(x, y)
 
     # 初始化一个布尔数组，用于标记在圆外的点
@@ -135,6 +142,31 @@ if __name__ == '__main__':
         u_numerical = np.full_like(X, np.nan)
         u_numerical[outside] = numerical_values.flatten()
         U_numerical.append(u_numerical)
+
+    # 计算全部误差 start
+    all_points = pick_point(x, y, t)
+    all_point = torch.tensor(all_points, requires_grad=False)
+    A = np.zeros((len(all_point), Nx * Ny * Nt * M))
+    for k in range(Nx):
+        for j in range(Ny):
+            for n in range(Nt):
+                M_begin = (k * Ny * Nt + j * Nt + n) * M
+                out = models[k][j][n](all_point)
+                values = out.detach().numpy()
+                A[:, M_begin: M_begin + M] = values
+
+    all_numerical_values = np.dot(A, w)
+    all_true_values = u_real(all_points[:, [0]], all_points[:, [1]], all_points[:, [2]])
+
+    all_epsilon = np.abs(all_true_values - all_numerical_values)
+    L_inf_all = np.max(all_epsilon)
+    L_2_all = np.sqrt(np.sum(all_epsilon ** 2) / len(all_epsilon))
+    print(
+        'Nx={:d},Ny={:d},Nt={:d},M={:d},test_Qx={:d},test_Qy={:d},test_Qt={:d}'.format(Nx, Ny, Nt, M, test_Qx, test_Qy,
+                                                                                       test_Qt))
+    print(datetime.now(), 'L_inf_all={:.2e}'.format(L_inf_all), 'L_2_all={:.2e}'.format(L_2_all))
+
+    # 计算全部误差 end
 
     print(datetime.now(), "Main end")
     plot(X, Y, U_numerical)
