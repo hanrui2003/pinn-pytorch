4f2e089339ceddf51443913357e95f5db89db87e
liqingchun <merlini1988@126.com>
2025-03-18 11:45:44 +0800

    MLDA

---
+++ st_rfm_lite/oned_diff_pinn.py
@@ -0,0 +1,156 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+from torch.utils.data import Dataset, DataLoader
+from datetime import datetime
+import sys
+from pyDOE import lhs
+
+torch.set_default_dtype(torch.float)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+Pi = np.pi
+nu = 0.01
+
+X_min = 0.0
+X_max = 5.0
+T_min = 0.0
+T_max = 1.0
+
+Nx = 5
+# t维度划分的区间数
+Nt = 2
+# 每个局部局域的特征函数数量
+M = 150
+# x维度每个区间的配点数，Qx+1
+Qx = 3
+# t维度每个区间的配点数，Qt+1
+Qt = 3
+
+
+def u_real(x, t):
+    u0 = 2 * np.cos(Pi * x + Pi / 5) + 3 / 2 * np.cos(2 * Pi * x - 3 * Pi / 5)
+    u1 = 2 * np.cos(Pi * t + Pi / 5) + 3 / 2 * np.cos(2 * Pi * t - 3 * Pi / 5)
+
+    u = u0 * u1
+
+    return u
+
+
+def f_real(x, t):
+    u0 = 2 * np.cos(Pi * x + Pi / 5) + 3 / 2 * np.cos(2 * Pi * x - 3 * Pi / 5)
+    u1 = 2 * np.cos(Pi * t + Pi / 5) + 3 / 2 * np.cos(2 * Pi * t - 3 * Pi / 5)
+
+    u1t = -2 * Pi * np.sin(Pi * t + Pi / 5) - 3 * Pi * np.sin(2 * Pi * t - 3 * Pi / 5)
+    ut = u0 * u1t
+
+    u0xx = -2 * Pi * Pi * np.cos(Pi * x + Pi / 5) - 6 * Pi * Pi * np.cos(2 * Pi * x - 3 * Pi / 5)
+    uxx = u0xx * u1
+
+    f = ut - nu * uxx
+
+    return f
+
+
+class DiffNet(nn.Module):
+    def __init__(self, layers):
+        super().__init__()
+        self.nu = torch.tensor(0.01).float().to(device)
+        self.activation = nn.Tanh()
+        self.loss_func = nn.MSELoss(reduction='mean')
+
+        self.linears = nn.ModuleList([nn.Linear(layers[j], layers[j + 1]) for j in range(len(layers) - 1)])
+
+        for linear in self.linears:
+            nn.init.xavier_normal_(linear.weight.data)
+            nn.init.zeros_(linear.bias.data)
+
+    def forward(self, a):
+        for j in range(len(self.linears) - 1):
+            z = self.linears[j](a)
+            a = self.activation(z)
+        return self.linears[-1](a)
+
+    def loss_label(self, y, label):
+        """
+        带标签的点
+        """
+        hat = self.forward(y)
+        return self.loss_func(label, hat)
+
+    def loss_pde(self, y_pde, label_pde):
+        """
+        PDE的点
+        """
+        y_pde.requires_grad = True
+        u_hat = self.forward(y_pde)
+
+        u_x_t = autograd.grad(u_hat, y_pde, torch.ones_like(u_hat), create_graph=True)[0]
+        u_x = u_x_t[:, [0]]
+        u_t = u_x_t[:, [1]]
+
+        u_xx_xt = autograd.grad(u_x, y_pde, torch.ones_like(u_x), create_graph=True)[0]
+        u_xx = u_xx_xt[:, [0]]
+
+        return self.loss_func(u_t - self.nu * u_xx, label_pde)
+
+    def loss(self, y_ic_bc, label_ic_bc, y_pde, label_pde):
+        loss_ic_bc = self.loss_label(y_ic_bc, label_ic_bc)
+        loss_pde = self.loss_pde(y_pde, label_pde)
+        return loss_ic_bc + loss_pde
+
+
+if "__main__" == __name__:
+    # torch.manual_seed(123)
+    # np.random.seed(123)
+
+    x = np.linspace(X_min, X_max, Nx * Qx + 1)
+    t = np.linspace(T_min, T_max, Nt * Qt + 1)
+    X, T = np.meshgrid(x, t)
+    # 用于计算pde损失的点
+    y_train_pde = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+    y_label_pde = f_real(y_train_pde[:, [0]], y_train_pde[:, [1]])
+
+    # 带标签值的点，即有真解的点
+    ic_points = np.hstack((X[0][:, None], T[0][:, None]))
+    left_bc_points = np.hstack((X[:, 0][:, None], T[:, 0][:, None]))
+    right_bc_points = np.hstack((X[:, -1][:, None], T[:, -1][:, None]))
+    bc_points = np.vstack((left_bc_points, right_bc_points))
+    y_train_ic_bc = np.vstack((ic_points, bc_points))
+    y_label_ic_bc = u_real(y_train_ic_bc[:, [0]], y_train_ic_bc[:, [1]])
+
+    # 把训练数据转为tensor
+    y_train_ic_bc = torch.from_numpy(y_train_ic_bc).float().to(device)
+    y_label_ic_bc = torch.from_numpy(y_label_ic_bc).float().to(device)
+    y_train_pde = torch.from_numpy(y_train_pde).float().to(device)
+    y_label_pde = torch.from_numpy(y_label_pde).float().to(device)
+
+    layers = [2, 32, 32, 32, 32, 1]
+    model = DiffNet(layers)
+    model.to(device)
+    print("model:\n", model)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, amsgrad=False)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2000, gamma=0.5)
+
+    # 记录训练开始时间
+    start_time = datetime.now()
+    print("Training started at:", start_time.strftime("%Y-%m-%d %H:%M:%S"))
+    for epoch in range(40000):
+        loss = model.loss(y_train_ic_bc, y_label_ic_bc, y_train_pde, y_label_pde)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        if (epoch + 1) % 100 == 0:
+            print(datetime.now(), 'epoch :', epoch + 1, 'lr :', optimizer.param_groups[0]['lr'], 'loss :', loss.item())
+
+    torch.save(model, 'oned_diff_pinn_01.pt')
+
+    # 训练结束后记录结束时间并计算总时间
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    print("Training ended at:", end_time.strftime("%Y-%m-%d %H:%M:%S"))
+    print("Elapsed time: ", elapsed_time)
---
+++ st_rfm_lite/oned_diff_pinn_test.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import itertools
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+import math
+from conv_diff_1d_da_pinn import u_real, ConvDiffNet, X_min, X_max, T_min, T_max, Nx, Nt, M, Qx, Qt
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+def plot(X1, T1, U1, X2, T2, U2):
+    """
+    同时绘制PDE的数值解和神经网络解，上面数值解，下面神经网络解。
+    """
+    min_value = np.min([U1, U2])
+    max_value = np.max([U1, U2])
+    # 创建一个 Figure 对象，并设置子图布局
+    fig = plt.figure(figsize=(12, 8))
+    ax1 = fig.add_subplot(221)
+    # ax1.set_ylim
+    ax2 = fig.add_subplot(222, projection='3d')
+    ax3 = fig.add_subplot(223)
+    ax4 = fig.add_subplot(224, projection='3d')
+
+    cp1 = ax1.contourf(T1, X1, U1, 20, cmap="rainbow", vmin=min_value, vmax=max_value)
+    fig.colorbar(cp1, ax=ax1)
+    ax1.set_title('u(x,t)')
+    ax1.set_xlabel('t')
+    ax1.set_ylabel('x')
+
+    ax2.plot_surface(T1, X1, U1, cmap="rainbow", vmin=min_value, vmax=max_value)
+    ax2.set_xlabel('t')
+    ax2.set_ylabel('x')
+    ax2.set_zlabel('u(x,t)')
+    ax2.set_zlim(min_value, max_value)
+
+    cp3 = ax3.contourf(T2, X2, U2, 20, cmap="rainbow", vmin=min_value, vmax=max_value)
+    fig.colorbar(cp3, ax=ax3)
+    ax3.set_title('PINN(x,t)')
+    ax3.set_xlabel('t')
+    ax3.set_ylabel('x')
+
+    ax4.plot_surface(T2, X2, U2, cmap="rainbow", vmin=min_value, vmax=max_value)
+    ax4.set_xlabel('t')
+    ax4.set_ylabel('x')
+    ax4.set_zlabel('PINN(x,t)')
+    ax4.set_zlim(min_value, max_value)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    print(datetime.now(), "Main start")
+    model = torch.load('conv_diff_1d_da_pinn_01.pt', map_location=torch.device('cpu'))
+
+    test_Qx = 2 * Qx
+    test_Qt = 2 * Qt
+
+    x = np.linspace(X_min, X_max, Nx * test_Qx + 1)
+    t = np.linspace(T_min, T_max, Nt * test_Qt + 1)
+    X, T = np.meshgrid(x, t)
+    points = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+    point = torch.tensor(points, dtype=torch.float, requires_grad=False)
+    numerical_values = model(point).detach().numpy()
+    U_numerical = numerical_values.reshape((X.shape[0], X.shape[1]))
+    U_true = u_real(X, T)
+    epsilon = np.abs(U_true - U_numerical)
+    L_inf = np.max(epsilon)
+    L_2 = np.sqrt(np.sum(epsilon ** 2) / len(epsilon))
+
+    print('********************* ERROR *********************')
+    print('Nx={:d},Nt={:d},M={:d},Qx={:d},Qt={:d}'.format(Nx, Nt, M, Qx, Qt))
+    print(datetime.now(), 'L_inf={:.2e}'.format(L_inf), 'L_2={:.2e}'.format(L_2))
+    print(datetime.now(), "Main end")
+
+    plot(X, T, U_true, X, T, U_numerical)
